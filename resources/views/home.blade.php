ad70bb60c425f136cfb1f1821e19ee9df2cc8b71
Sebastian-Rodriguez <sebasrz.rcia@gmail.com>
2020-06-21 22:42:25 -0300

    Agregar sistema de autentificacion de usuario y modificacion de vista home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,29 @@
 	<title>Web Oficios</title>
 </head>
 <body>
+	{{-- GUEST = invitado
+		Pregunta si el usuario es un invitado (no está logueado) y en 
+		caso de que se cumpla esa condición se muestra los links
+		para logueo y registro
+	 --}}
+	@guest
+		<ul>
+			<li><a href="/login">Ingresar</a></li>
+			<li><a href="/register">Registrate</a></li>
+		</ul>
+	@endguest
+	{{-- Si el usuario está logueado muestra el link para crear/modificar perfil 
+		y para salir
+	--}}
+	@auth
+		<ul>
+			<li><a href="#">Crear/Modificar Perfil</a></li>
+			<li><a href="#" onclick="
+				event.preventDefault();
+				document.getElementById('form-post').submit();
+				">Salir</a></li>
+		</ul>
+	@endauth
 	<form action="/buscar" method="POST">
 		<input type="hidden" name="_token" value="{{ csrf_token() }}">
 		<label for="categoria">Selecciona un Rubro</label>
@@ -33,5 +56,14 @@
 		</li>
 		@endforeach
 	</ul>
+
+	{{-- Formulario oculto que se ejecuta al hacer click en "Salir" 
+		* Redirige a la ruta /logout con el método POST, ahí el controlador
+		  del logout borra los datos de sesión 
+		* Rediirige a la ruta /home con la sesión cerrada
+	--}}
+	<form id="form-post" action="/logout" method="POST" style="display: none;">
+		@csrf
+	</form>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 ## 
 Route::get('/', 'HomeController@ListarTodos');
 
 Route::post('/buscar', 'HomeController@ListarFiltrados');
 
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/profesional/{id}', 'ProfesionalController@mostrarPerfil');
+
+Auth::routes();
+
+
