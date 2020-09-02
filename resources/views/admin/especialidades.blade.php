fb4d4ec33be2124c3d5a81f4f4d17d1cf690e790
Joselina <joselynr794@gmail.com>
2020-09-02 17:43:51 -0300

    nuevas vistas

--- resources/views/admin/especialidades.blade.php
+++ resources/views/admin/especialidades.blade.php
@@ -62,47 +62,4 @@
     </div>
 </section>
 
-
- {{-- agregar nueva la especialidad  --}}   
-    <div>
-        <form id="contact-form" name="contact-form" action="/admin/storeEspecialidad" method="POST"
-            enctype="multipart/form-data">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="md-form mb-3">
-                        <label for="oficio" class="">Oficio</label>
-                        {{-- debe mandarse el nombre d la profesión a la que se agregara la especialidad  --}}
-                        <select class="browser-default custom-select" name="oficio" required>
-							<option value="" selected disabled>Seleccionar un oficio</option>
-							@foreach ($listaoficio as $oficio)
-							    <option value="{{ $oficio['Oficio']->id}}">{{ $oficio['Oficio']->nombre }}</option>
-						    @endforeach
-						</select>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="md-form mb-3">
-                        <label for="nombre" class="">Nombre de la Espacialiodad</label>
-                        <input type="text" id="nombre" name="nombre" value=''
-                            class="form-control" required>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="md-form mb-3">
-                        <label for="descripcion" class="">Descripcion de la Espacialiodad</label>
-                        <input type="text" id="descripcion" name="descripcion" value=''
-                            class="form-control" required>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="centrar mt-5">
-                        <button class="btn" type="submit">Enviar</button>
-                    </div>
-                </div>
-
-            </div>
-    </div>
-    @csrf
-    </form>
-    <div>
-    @endsection
+@endsection
