35ec9985b1fd970c0c0d745ee53c6af9f15966cd
Somos 404 <somos404studio@gmail.com.ar>
2020-09-04 15:13:22 -0300

    home, esp

--- resources/views/admin/especialidades.blade.php
+++ resources/views/admin/especialidades.blade.php
@@ -25,45 +25,17 @@
                     <td> {{ $oficio['Oficio']->nombre }}</td>
                     <td>
                         @if ($oficio['Especialidades'])
-<<<<<<< HEAD
-                             @foreach ($oficio['Especialidades'] as $especialidad)
-                                
-                                     <p>  
-                                        <a class="btn-descripcion" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-                                        {{ $especialidad->nombre }}
-=======
                              @foreach ($oficio['Especialidades'] as $especialidad)                                
                                      <div>  
                                         <a class="btn-descripcion" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
                                          {{ $especialidad->nombre }}
->>>>>>> 2eb51f05f6c758ac0009fd87ec276df760e02de7
                                             <a class="icon-action"  href="{{ route('especialidad.destroyEspecialidad', $especialidad->id) }}" onclick="return confirm('¿Desea eliminar la especialidad?')" onkeypress="return confirm('¿Desea eliminar la especialidad?')" data-toggle="tooltip" data-placement="top" title="Eliminar">
                                             <i class="fa fa-remove"></i>
                                          </a>
                                         </a>
                                         
-<<<<<<< HEAD
-                                    </p>
-                                   
-                                    @if ($especialidad->descripcion)
-                                             <div class="collapse" id="collapseExample">
-                                                <div class="card card-body">
-                                                    <li>
-                                                    {{ $especialidad->nombre }}
-                                                         {{ $especialidad->descripcion }}
-                                                    </li>
-                                                    </div>
-                                            </div>
-                                  
-                                    
-                                    @endif
-                                            
-                         
-                                         
-=======
                                     </div>
                                         
->>>>>>> 2eb51f05f6c758ac0009fd87ec276df760e02de7
                               @endforeach                            
                         @endif
                     </td>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,22 @@
 		{!! session('message') !!}
 	</div>
 @endif
+
+ <!-- Page Header -->
+ <header class="masthead" style="background-image: url('/assets/img/Slide3.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1>BLOG</h1>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
 <section class="py-3">
 	<div class="container text-center">
 		<h2 class="my-4"> ¿Qué estás buscando?</h2>
