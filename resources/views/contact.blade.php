412247c9480856be7d390e119926c2498a9419d1
Joselina <joselynr794@gmail.com>
2020-09-08 20:52:56 -0300

    contact 3

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,45 +4,50 @@
 
 @section('content')
 
-<section class="page-section my-3" id="contact">
+<section class="page-section" id="contact">
     <div class="container">
         <div class="row">
             <div class="col-md-6">
                 <div class="contacto">
                     <h1 class="section-heading">Contacto</h1>
-                    <h3 class="section-subheading">¿Tenés alguna pregunta? Comunícate con nosotros, responderemos tan pronto como sea posible.</h3>
+                    <h3 class="section-subheading my-4">¿Tenés alguna pregunta? Comunícate con nosotros, responderemos tan pronto como sea posible.</h3>
                 </div>
                 <form id="contactForm" name="sentMessage" novalidate="novalidate">
                     <div class="row align-items-stretch mb-2">
                         <div class="col-md-12">
                             <div class="form-group">
-                                <input class="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." />
+                                <label for="">Tu nombre *</label>
+                                <input class="form-control" id="name" type="text" required="required" data-validation-required-message="Please enter your name." />
                                 <p class="help-block text-danger"></p>
                             </div>
                             <div class="form-group">
-                                <input class="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address." />
+                                <label for="">Correo electrónico *</label>
+                                <input class="form-control" id="email" type="email" required="required" data-validation-required-message="Please enter your email address." />
                                 <p class="help-block text-danger"></p>
                             </div>
                             <div class="form-group mb-md-0">
-                                <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number." />
+                                <label for="">Asunto</label>
+                                <input class="form-control" id="asunto" type="asunto"  required="required" data-validation-required-message="Please enter your phone number." />
                                 <p class="help-block text-danger"></p>
                             </div>
                         </div>
                         <div class="col-md-12">
                             <div class="form-group form-group-textarea mb-md-0">
-                                <textarea class="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                                <label for="">Mensaje</label>
+                                <textarea class="form-control" id="message" required="required" data-validation-required-message="Please enter a message."></textarea>
                                 <p class="help-block text-danger"></p>
+                                <small>*Obligatorio</small>
                             </div>
                         </div>
                     </div>
                     <div class="text-right mb-5">
                         <div id="success"></div>
-                        <button class="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">Send Message</button>
+                        <button class="btn hollow" id="sendMessageButton" type="submit">Enviar</button>
                     </div>
                 </form>
             </div>
-            <div class="col-md-6">
-                skjdhskldjñ
+            <div class="col-md-6 img-contact">
+               <img class="" src="/img/contact.png" alt="">
             </div>
         </div>
     </div>
