5231444e35912b85ad69380c11fb01a1775df30d
pedro <nassiffpedro@gmail.com>
2020-10-21 22:43:28 -0300

    nav paneles

--- resources/views/user/editarPerfil.blade.php
+++ resources/views/user/editarPerfil.blade.php
@@ -23,106 +23,120 @@
 			
 			<div class="col-sm-8">
 				<div class="menu-perfil">
-					<ul class="nav nav-tabs nav-tab-perfil">
-						<li class="nav-item">
-							<a class="nav-link .nav-perfil active" href="#">Configuración general</a>
+					<div role="tabpanel">
+					<ul class="nav nav-tabs nav-tab-perfil" role="tablist">
+						<li class="nav-item" role="presentacion" id="nav-perfil">
+							<a class="nav-link nav-perfil active" href="#configuracionGeneral" aria-controls="configuracionGeneral" data-toggle="tab" role="tab">Configuración general</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link nav-perfil" href="#">Seguridad</a>
+						<li class="nav-item" role="presentacion">
+							<a class="nav-link nav-perfil" href="#seguridad"  aria-controls="seguridad" data-toggle="tab" role="tab">Seguridad</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link nav-perfil" href="#">Documentos</a>
+						<li class="nav-item" role="presentacion">
+							<a class="nav-link nav-perfil" href="#documentos"  aria-controls="documentos" data-toggle="tab" role="tab">Documentos</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link nav-perfil" href="#">Notificaciones</a>
+						<li class="nav-item" role="presentacion" >
+							<a class="nav-link nav-perfil" href="#notificaciones"  aria-controls="notificaciones" data-toggle="tab" role="tab">Notificaciones</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link nav-perfil" href="#">Mensajes</a>
+						<li class="nav-item" role="presentacion">
+							<a class="nav-link nav-perfil" href="#mensajes"  aria-controls="mensajes" data-toggle="tab" role="tab">Mensajes</a>
 						</li>		
 					</ul>
-				</div>
-				<div class="row">
-					<div class="col-md-10">
-						<form action="/perfil/actualizar/{{ auth()->user()->id }}" method="POST">
+					<div class="tab-content">
+						<div role="tab-panel" class="tab-pane active" id="configuracionGeneral">
 							<div class="row">
-
-								<div class="col-md-6">
-									<div class="md-form mb-3">
-										<label for="nombre" class="">Nombres</label>
-										<input type="text" name="nombre" value="profesionalnombre" class="form-control" required="">
+								<div class="col-md-10">
+									<form action="/perfil/actualizar/{{ auth()->user()->id }}" method="POST">
+										<div class="row">
+											<div class="col-md-6">
+												<div class="md-form mb-3">
+													<label for="nombre" class="">Nombres</label>
+														<input type="text" name="nombre" value="profesionalnombre" class="form-control" required="">
+												</div>
+											</div>
+												<div class="col-md-6">
+													<div class="md-form mb-3">
+														<label for="apellido" class="">Apellido</label>
+														<input type="text" id="apellido" name="apellido" value="profesionalapellido " class="form-control" required>
+													</div>
+												</div>
+												<div class="col-md-6">
+													<div class="md-form mb-3">
+														<label for="dni" class="">DNI</label>
+														<input type="text" id="dni" name="dni" value="profesionaldni" class="form-control" required>
+													</div>
+												</div>
+												<div class="col-md-6">
+													<div class="md-form mb-3">
+														<label for="telefono" class="">Teléfono</label>
+														<input type="text" id="telefono" name="telefono" value="profesionaltelefono " class="form-control" required>
+													</div>
+												</div>
+												<div class="col-md-6">
+													<div class="md-form mb-3">
+														<label for="email" class="">Email</label>
+														<input type="text" id="email" name="email" value="" class="form-control" required>
+													</div>
+												</div>
+												<div class="col-md-6">
+													<div class="md-form mb-3">
+														<label for="direccion" class="">Dirección</label>
+														<input type="text" id="direccion" name="direccion" value="profesionaldireccion " class="form-control" required>
+													</div>
+												</div>
+												<div class="col-md-6">
+													<div class="md-form mb-3">
+														<label for="localidad" class="">Localidad</label>
+														<select class="browser-default custom-select" name="localidad_id">
+																<option value=""> localidad->nombre </option>
+														</select>
+													</div>
+												</div>
+												<div class="col-md-6">
+													<div class="md-form mb-3">
+														<label for="rubro" class="">Tipo de servicio (Rubro)</label>
+														<select class="browser-default custom-select" name="rubro_id">
+																<option value="">rubro->nombre</option>
+														</select>
+													</div>
+												</div>
+												<div class="col-md-6">
+													<div class="md-form mb-3">
+														<label for="web" class="">Sitio Web</label>
+														<input type="text" id="web" name="web" value="profesional->web" class="form-control" required>
+													</div>
+												</div>
+												<!--Grid column-->
+										</div>
+								</div>	
+											<div class="col-md-10 centrar-der">
+												<button class="btn btn-own mx-auto" type="submit">Guardar</button>
+											</div>
+											
+											@csrf
+										</form>
 									</div>
 								</div>
-
-								<div class="col-md-6">
-									<div class="md-form mb-3">
-										<label for="apellido" class="">Apellido</label>
-										<input type="text" id="apellido" name="apellido" value="profesionalapellido " class="form-control" required>
-									</div>
-								</div>
-
-								<div class="col-md-6">
-									<div class="md-form mb-3">
-										<label for="dni" class="">DNI</label>
-										<input type="text" id="dni" name="dni" value="profesionaldni" class="form-control" required>
-									</div>
-								</div>
-
-								<div class="col-md-6">
-									<div class="md-form mb-3">
-										<label for="telefono" class="">Teléfono</label>
-										<input type="text" id="telefono" name="telefono" value="profesionaltelefono " class="form-control" required>
-									</div>
-								</div>
-
-								<div class="col-md-6">
-									<div class="md-form mb-3">
-										<label for="email" class="">Email</label>
-										<input type="text" id="email" name="email" value="" class="form-control" required>
-									</div>
-								</div>
-
-								<div class="col-md-6">
-									<div class="md-form mb-3">
-										<label for="direccion" class="">Dirección</label>
-										<input type="text" id="direccion" name="direccion" value="profesionaldireccion " class="form-control" required>
-									</div>
-								</div>
-
-								<div class="col-md-6">
-									<div class="md-form mb-3">
-										<label for="localidad" class="">Localidad</label>
-										<select class="browser-default custom-select" name="localidad_id">
-												<option value=""> localidad->nombre </option>
-										</select>
-									</div>
-								</div>
-
-								<div class="col-md-6">
-									<div class="md-form mb-3">
-										<label for="rubro" class="">Tipo de servicio (Rubro)</label>
-										<select class="browser-default custom-select" name="rubro_id">
-												<option value="">rubro->nombre</option>
-										</select>
-									</div>
-								</div>
-
-								<div class="col-md-6">
-									<div class="md-form mb-3">
-										<label for="web" class="">Sitio Web</label>
-										<input type="text" id="web" name="web" value="profesional->web" class="form-control" required>
-									</div>
-								</div>
-								<!--Grid column-->
+							<div role="tab-panel" class="tab-pane" id="seguridad">
+								<h3>Seguridad</h3>
+								<p>Seguridad  seguridadseguridadseguridadseguridad seguridad seguridad seguridad</p>
 							</div>
-					</div>	
-					<div class="col-md-10 centrar-der">
-						<button class="btn btn-own mx-auto" type="submit">Guardar</button>
+							<div role="tab-panel" class="tab-pane" id="documentos">
+								<h3>Documentos</h3>
+								<p>Seguridad  seguridadseguridadseguridadseguridad seguridad seguridad seguridad</p>
+							</div>
+							<div role="tab-panel" class="tab-pane" id="notificaciones">
+								<h3>Notificaciones</h3>
+								<p>Seguridad  seguridadseguridadseguridadseguridad seguridad seguridad seguridad</p>
+							</div>
+							<div role="tab-panel" class="tab-pane" id="mensajes">
+								<h3>Mensajes</h3>
+								<p>Seguridad  seguridadseguridadseguridadseguridad seguridad seguridad seguridad</p>
+							</div>
+						</div>
+						
 					</div>
-							
-							@csrf
-						</form>
 				</div>
-			</div>
+				
 		</div>
 	</div>
 </section>					
