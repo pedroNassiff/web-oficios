c599efb829b0455d76ba0d43ce851416fe368968
pedro <nassiffpedro@gmail.com>
2020-10-21 22:08:44 -0300

    nav del perfil

--- resources/views/user/editarPerfil.blade.php
+++ resources/views/user/editarPerfil.blade.php
@@ -20,8 +20,27 @@
         			<input type="file" class="text-center center-block file-upload">
 				</div>
 			</div>
-
+			
 			<div class="col-sm-8">
+				<div class="menu-perfil">
+					<ul class="nav nav-tabs nav-tab-perfil">
+						<li class="nav-item">
+							<a class="nav-link .nav-perfil active" href="#">Configuración general</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link nav-perfil" href="#">Seguridad</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link nav-perfil" href="#">Documentos</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link nav-perfil" href="#">Notificaciones</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link nav-perfil" href="#">Mensajes</a>
+						</li>		
+					</ul>
+				</div>
 				<div class="row">
 					<div class="col-md-10">
 						<form action="/perfil/actualizar/{{ auth()->user()->id }}" method="POST">
@@ -30,42 +49,42 @@
 								<div class="col-md-6">
 									<div class="md-form mb-3">
 										<label for="nombre" class="">Nombres</label>
-										<input type="text" name="nombre" value="{{ $profesional ?? ''->nombre }}" class="form-control" required="">
+										<input type="text" name="nombre" value="profesionalnombre" class="form-control" required="">
 									</div>
 								</div>
 
 								<div class="col-md-6">
 									<div class="md-form mb-3">
 										<label for="apellido" class="">Apellido</label>
-										<input type="text" id="apellido" name="apellido" value="{{ $profesional ?? ''->apellido }}" class="form-control" required>
+										<input type="text" id="apellido" name="apellido" value="profesionalapellido " class="form-control" required>
 									</div>
 								</div>
 
 								<div class="col-md-6">
 									<div class="md-form mb-3">
 										<label for="dni" class="">DNI</label>
-										<input type="text" id="dni" name="dni" value="{{ $profesional ?? ''->dni }}" class="form-control" required>
+										<input type="text" id="dni" name="dni" value="profesionaldni" class="form-control" required>
 									</div>
 								</div>
 
 								<div class="col-md-6">
 									<div class="md-form mb-3">
 										<label for="telefono" class="">Teléfono</label>
-										<input type="text" id="telefono" name="telefono" value="{{ $profesional ?? ''->telefono }}" class="form-control" required>
+										<input type="text" id="telefono" name="telefono" value="profesionaltelefono " class="form-control" required>
 									</div>
 								</div>
 
 								<div class="col-md-6">
 									<div class="md-form mb-3">
 										<label for="email" class="">Email</label>
-										<input type="text" id="email" name="email" value="{{ auth()->user()->email }}" class="form-control" required>
+										<input type="text" id="email" name="email" value="" class="form-control" required>
 									</div>
 								</div>
 
 								<div class="col-md-6">
 									<div class="md-form mb-3">
 										<label for="direccion" class="">Dirección</label>
-										<input type="text" id="direccion" name="direccion" value="{{ $profesional ?? ''->direccion }}" class="form-control" required>
+										<input type="text" id="direccion" name="direccion" value="profesionaldireccion " class="form-control" required>
 									</div>
 								</div>
 
@@ -73,9 +92,7 @@
 									<div class="md-form mb-3">
 										<label for="localidad" class="">Localidad</label>
 										<select class="browser-default custom-select" name="localidad_id">
-											@foreach ($localidades as $localidad)
-												<option value="{{ $localidad->id }}">{{ $localidad->nombre }}</option>
-											@endforeach
+												<option value=""> localidad->nombre </option>
 										</select>
 									</div>
 								</div>
@@ -84,9 +101,7 @@
 									<div class="md-form mb-3">
 										<label for="rubro" class="">Tipo de servicio (Rubro)</label>
 										<select class="browser-default custom-select" name="rubro_id">
-											@foreach ($rubros as $rubro)
-												<option value="{{ $rubro->id }}">{{ $rubro->nombre }}</option>
-											@endforeach
+												<option value="">rubro->nombre</option>
 										</select>
 									</div>
 								</div>
@@ -94,7 +109,7 @@
 								<div class="col-md-6">
 									<div class="md-form mb-3">
 										<label for="web" class="">Sitio Web</label>
-										<input type="text" id="web" name="web" value="{{ $profesional ?? ''->web }}" class="form-control" required>
+										<input type="text" id="web" name="web" value="profesional->web" class="form-control" required>
 									</div>
 								</div>
 								<!--Grid column-->
