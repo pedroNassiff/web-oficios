a1498df6e025174f5126cfea02cfff184d5ab9d9
pedro <nassiffpedro@gmail.com>
2020-11-06 23:33:32 -0300

    perfil-config general

--- resources/views/user/editarPerfil.blade.php
+++ resources/views/user/editarPerfil.blade.php
@@ -13,13 +13,7 @@
 		</div>
 
 		<div class="row">
-			<div class="col-sm-4"><!--left col-->					
-				<div class="text-center">
-					<img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" class="avatar img-circle img-thumbnail" alt="avatar">
-					<h6>Cambiar foto de perfil</h6>
-        			<input type="file" class="text-center center-block file-upload">
-				</div>
-			</div>
+			
 			
 			<div class="col-sm-8">
 				<div class="menu-perfil">
@@ -47,18 +41,31 @@
 								<div class="col-md-10">
 									<form action="/perfil/actualizar/{{ auth()->user()->id }}" method="POST">
 										<div class="row">
-											<div class="col-md-6">
-												<div class="md-form mb-3">
-													<label for="nombre" class="">Nombres</label>
-														<input type="text" name="nombre" value="profesionalnombre" class="form-control" required="">
+											<div class="col-md-12 col-1-perfil">
+												<div class="col-md-6"><!--left col-->					
+													<div class="text-center cont-fperfil">
+														<img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" class="avatar img-circle img-thumbnail" alt="avatar">
+														<h6>Cambiar foto de perfil</h6>
+														<input type="file" class="text-center center-block file-upload">
+													</div>
 												</div>
-											</div>
+												<div class="col-md-6">
+													<div class="md-form mb-3">
+														<label for="nombre" class="">Nombres</label>
+															<input type="text" name="nombre" value="profesionalnombre" class="form-control" required="">
+													</div>
+												</div>
 												<div class="col-md-6">
 													<div class="md-form mb-3">
 														<label for="apellido" class="">Apellido</label>
 														<input type="text" id="apellido" name="apellido" value="profesionalapellido " class="form-control" required>
 													</div>
 												</div>
+											</div>
+										</div>
+										<div class="col-form-perf">
+										<div class="row">
+											<div class="col-md-12 col-1-perfil">
 												<div class="col-md-6">
 													<div class="md-form mb-3">
 														<label for="dni" class="">DNI</label>
@@ -71,6 +78,10 @@
 														<input type="text" id="telefono" name="telefono" value="profesionaltelefono " class="form-control" required>
 													</div>
 												</div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-12 col-1-perfil">
 												<div class="col-md-6">
 													<div class="md-form mb-3">
 														<label for="email" class="">Email</label>
@@ -83,6 +94,10 @@
 														<input type="text" id="direccion" name="direccion" value="profesionaldireccion " class="form-control" required>
 													</div>
 												</div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-12 col-1-perfil">
 												<div class="col-md-6">
 													<div class="md-form mb-3">
 														<label for="localidad" class="">Localidad</label>
@@ -99,14 +114,17 @@
 														</select>
 													</div>
 												</div>
-												<div class="col-md-6">
+												
+											</div>
+												<div class="col-md-6" id="web-perfil">
 													<div class="md-form mb-3">
 														<label for="web" class="">Sitio Web</label>
 														<input type="text" id="web" name="web" value="profesional->web" class="form-control" required>
 													</div>
 												</div>
+												</div>
 												<!--Grid column-->
-										</div>
+									</div>
 								</div>	
 											<div class="col-md-10 centrar-der">
 												<button class="btn btn-own mx-auto" type="submit">Guardar</button>
