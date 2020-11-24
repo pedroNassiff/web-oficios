0226dd19c2a83a9f5ee6dc00e97fd4d8247ddc5e
Bran <bbogdanich42@gmail.com>
2020-11-24 12:08:38 -0300

    heroku

--- config/database.php
+++ config/database.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Str;
 
 
-/* $url = parse_url(getenv("DATABASE_URL"));
+$url = parse_url(getenv("DATABASE_URL"));
 
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
- */
+
 return [
 
     /*
@@ -23,7 +23,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -73,10 +73,10 @@ return [
 
         'pgsql' => array(
             'driver'   => 'pgsql',
-            'host'     => '$host',
-            'database' => '$database',
-            'username' => '$username',
-            'password' => '$password',
+            'host'     => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
